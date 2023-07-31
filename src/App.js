2c8b979f79dd002da743fc8c1e0cc78b0b809ddf
Hridoy <ridoy.java@gmail.com>
2023-07-31 14:34:03 +0600

    useState with prevState

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import FuctionalCounter from "./components/FuctionalCounter";
+import HookCounter from "./components/HookCounter";
 
 function App() {
   return (
     <div className="App">
-      <FuctionalCounter />
+      {/* <FuctionalCounter /> */}
+      <HookCounter />
     </div>
   );
 }
