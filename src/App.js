2f4159e4ac2c48216502dda4fa88194c85eaa1a8
Hridoy <ridoy.java@gmail.com>
2023-07-31 15:08:34 +0600

    useState with object

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import FuctionalCounter from "./components/FuctionalCounter";
 import HookCounter from "./components/HookCounter";
+import UseStateWithObject from "./components/UseStateWithObject";
 
 function App() {
   return (
     <div className="App">
       {/* <FuctionalCounter /> */}
-      <HookCounter />
+      <UseStateWithObject />
     </div>
   );
 }
